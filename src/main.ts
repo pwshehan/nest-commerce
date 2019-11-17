90113958eacf380d5393c97dc2ca772fa603a9df
Shehan Pramitha <pwshehan@gmail.com>
2019-11-17 19:00:10 +0530

    ready to deploy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express'
 import { AppModule } from './app.module';
+import * as Express from 'express';
+
+const server = Express();
+server.get('/', (req, res) => res.send('ok'));
+server.get('/_ah/health', (req, res) => res.send('ok'));
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
